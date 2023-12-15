1645af9c457c68037be61ef0e07a1bf5a2235adf
Nick Sette <njs2394@gmail.com>
2023-12-14 21:47:59 -0500

    shoe and book

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size) -> None:
+        self.brand = brand
+        if isinstance(size, int):
+            self.size = size
+        else:
+            print("size must be an integer")
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.getter
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        self.condition = 'New'
+        print("Your shoe is as good as new!")
+        return
